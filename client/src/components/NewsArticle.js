b5c0c7445a080c626422a0b2eebd034c0ba2e72f
aindrila-choudhuri <choudhuri.aindrila@gmail.com>
2021-02-26 09:23:54 +0530

    9/N image on error is added

--- client/src/components/NewsArticle.js
+++ client/src/components/NewsArticle.js
@@ -5,9 +5,14 @@ const NewsArticle = (props) => {
 
     const defaultImageURL = process.env.PUBLIC_URL + "/news.jpeg"
 
+    function addDefaultSrc(e){
+        e.target.src = defaultImageURL;
+        e.target.onerror = null;
+    }
+    
     return(
         <div data-testid="newsarticle" className="news" onClick={()=>window.location.href=news.url}>
-            <img className="image__desc" src={news.urlToImage ? news.urlToImage : defaultImageURL} />
+            <img className="image__desc" alt= "Alternate text" onError={addDefaultSrc} src={news.urlToImage ? news.urlToImage : defaultImageURL} />
             <h4 className="news__title">{news.title}</h4>
             <span className="news__author">{news.author}</span> <br />
             <span className="news__published">{new Date(news.publishedAt).toString()}</span>
